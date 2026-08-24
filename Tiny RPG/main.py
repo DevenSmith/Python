03e6e97df0ce8cd1700d450f96c66ff5c57f925b
Deven Smith <sbob322@gmail.com>
2026-08-24 16:08:50 -0700

    Update main.py
    adding list of acceptable classes

--- Tiny RPG/main.py
+++ Tiny RPG/main.py
@@ -2,13 +2,19 @@ def greet_player(name: str) -> None:
     print(f"Welcome, {name}!")
 
 
-def create_character(name: str, character_class:str):
+def create_character(name: str, character_class: str) -> str:
     message = f"Character Created!\nName: {name}\nClass: {character_class}"
     return message
 
 
+acceptable_classes: list[str] = ["Warrior", "Mage", "Rogue"]
+
 player_name = input("What is your name? ")
 greet_player(player_name)
 player_class = input("What is your class? ")
-summary = create_character(player_name, player_class)
-print(summary)
+if player_class in acceptable_classes:
+    summary = create_character(player_name, player_class)
+    print(summary)
+else:
+    print("Your character class is invalid")
+
