1f3551d9ee11ec7bb293a46cb320ecb50c11a7eb
Deven Smith <sbob322@gmail.com>
2026-08-24 17:16:02 -0700

    Update main.py
    List and while loop

--- Tiny RPG/main.py
+++ Tiny RPG/main.py
@@ -7,14 +7,24 @@ def create_character(name: str, character_class: str) -> str:
     return message
 
 
+def print_available_classes(collection: list[str]) -> None:
+    print("Available Classes:")
+    for available_character_class in collection:
+        print("-", available_character_class)
+
+
 acceptable_classes: list[str] = ["Warrior", "Mage", "Rogue"]
 
 player_name = input("What is your name? ")
 greet_player(player_name)
-player_class = input("What is your class? ")
-if player_class in acceptable_classes:
-    summary = create_character(player_name, player_class)
-    print(summary)
-else:
+
+print_available_classes(acceptable_classes)
+
+while True:
+    player_class = input("What is your class? ")
+    if player_class in acceptable_classes:
+        break
     print("Your character class is invalid")
 
+summary = create_character(player_name, player_class)
+print(summary)
