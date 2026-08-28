5c42f72d34727706bafaceee20603ef4df463001
Deven Smith <sbob322@gmail.com>
2026-08-28 10:09:31 -0700

    Update App.tsx
    input

--- Tiny RPG/frontend/src/App.tsx
+++ Tiny RPG/frontend/src/App.tsx
@@ -7,13 +7,22 @@ type CharacterSummaryProps = {
 }
 
 function App() {
-  const characterName = 'Deven'
+  const [characterName, setCharacterName] = useState('Deven')
   const [startingHealth, setStartingHealth] = useState(120)
 
   return (
     <main>
       <h1>TinyRPG</h1>
       <p>Create your character</p>
+
+    <label htmlFor="character-name">Name</label>
+    <input
+      id="character-name"
+      type="text"
+      value={characterName}
+      onChange={(event) => setCharacterName(event.target.value)}
+    />
+
       <CharacterSummary
         name={characterName}
         startingHealth={startingHealth}
